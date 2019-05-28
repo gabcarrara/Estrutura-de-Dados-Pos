f48c397dccdc0987095c3ad9b2e8f11069c5f53b
CaferCruz <cafer_cruz@id.uff.br>
2019-05-28 20:37:21 -0300

    Leitura do dataset.

---
+++ main.h
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+//TO-DO: Modificar a entrada de arquivo, passando pelo terminal.
+void main(void){
+    FILE  * fp;
+
+    fp = fopen("dataset.txt", "r");
+
+
+
+    fclose();
+}
