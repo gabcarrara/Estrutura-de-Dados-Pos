482c1b0d69550cfc914b50d9e7b08c44e1ac72e2
Gabcarrara <gabcarrara@hotmail.com>
2019-05-28 21:22:28 -0300

    - Alteracao do struct info - Inclusao do metodo libera - Implementacao do metodo inicializa - Alteracao dos parametros do metodo insere

--- generic_tree.h
+++ generic_tree.h
@@ -1,6 +1,8 @@
 #ifndef _generic_tree_h
 #define _generic_tree_h
 
+include "poligono.h"
+
 typedef struct arvore {
     int cod;
     struct info * no;
@@ -12,18 +14,22 @@ typedef struct arvore {
 
 typedef struct info{
     int tipo;
-    void * info;
+    void * dados;
     float area;
 }TI;
 
 void imprime(TAG *arvore)
 
-TAG* insere(TAG* arvore, TAG* pai, TI* info)
+//TO-DO: Recebe struct do poligono e usa poligono_id para calcular a area correta.
+TAG* insere(TAG* arvore, TAG* pai, void *poligono, int poligono_id)
 
-TAG* inicializa()
+TAG* inicializa(){
+    return NULL;
+}
 
-TAG* remove(TAG* arvore, int cod, TAG* novo_pai)
+TAG* remove(TAG* arvore, int cod, int cod_novo_pai)
 
 TAG* busca(TAG* arvore, int cod)
 
+void libera(TAG* arvore)
 #endif
