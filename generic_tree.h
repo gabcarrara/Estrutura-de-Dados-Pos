bcab98ab3464d9110553312138055021a1a885f1
Gabcarrara <gabcarrara@hotmail.com>
2019-06-03 20:46:00 -0300

    -Implementacao do insere -Implementacao do busca -Diversos ajustes

--- generic_tree.h
+++ generic_tree.h
@@ -5,7 +5,8 @@ include "poligono.h"
 
 typedef struct arvore {
     int cod;
-    struct info * no;
+    int pai;
+    struct info *no;
     //Filho da esquerda
     struct arvore *filho; 
     //Filho da direita
@@ -14,14 +15,56 @@ typedef struct arvore {
 
 typedef struct info{
     int tipo;
-    void * dados;
+    void *dados;
     float area;
 }TI;
 
-void imprime(TAG *arvore)
+void imprime(TAG *arvore){
+    
+    
+}    
 
-//TO-DO: Recebe struct do poligono e usa poligono_id para calcular a area correta.
-TAG* insere(TAG* arvore, TAG* pai, void *poligono, int poligono_id)
+//TO-DO: Recebe struct do poligono e usar poligono_id para calcular a area correta.
+TAG* insere(TAG* arvore, int cod, int pai, void *poligono, enum_poli nome){
+    
+    if(pai == 0 && arvore!= NULL){
+        printf("Não é possível inserir uma nova raiz!");
+        return arvore;
+    }
+    
+    TAG *pai_aux =  busca(arvore, pai);
+    if(!pai_aux){
+        printf("Pai não encontrado");
+        reutrn arvore;
+    }
+    
+    TAG *novo = (TAG*)malloc(sizeof(TAG));
+    novo->cod = cod;
+    novo->pai = pai;
+    novo->no = (TI*)malloc(sizeof(TI));
+    novo->no->tipo = poligono_id;
+    novo->no->dados = poligono;
+    novo->no->area = calcula_area(poligono, nome);
+    novo->filho = NULL;
+    //insere como ultimo irmao
+    novo->irmao = NULL;
+    
+    if(pai == 0 ) return novo;
+    
+    
+    TAG *irmaos = pai_aux->filho;
+    
+    if(!irmaos){
+        pai_aux->filho = novo;
+        return arvore;
+    }
+    
+    while(irmaos->irmao) irmaos = irmaos->irmao;
+    irmaos->irmao = novo;
+    
+    printf("Nó inserido com sucesso!");
+    return arvore;
+}
 
 TAG* inicializa(){
     return NULL;
@@ -29,7 +72,14 @@ TAG* inicializa(){
 
 TAG* remove(TAG* arvore, int cod, int cod_novo_pai)
 
-TAG* busca(TAG* arvore, int cod)
+
+//Busco um no da arvore pelo seu codigo unico
+TAG* busca(TAG* arvore, int cod){
+    if(!arvore || arvore->cod == cod) return arvore;
+    TAG *irmao = busca(arvore->irmao, cod);
+    if(irmao) return irmao;
+    reutrn busca(arvore->filho);
+}
 
 void libera(TAG* arvore)
 #endif
