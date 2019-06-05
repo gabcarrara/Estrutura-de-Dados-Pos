94e4828cb01c05d20e2472394274478287fdcb8e
CaferCruz <cafer_cruz@id.uff.br>
2019-06-04 21:01:44 -0300

    - Criação do objeto poligono. -Restrição para criar uma unica instancia de poligono.h

--- poligono.h
+++ poligono.h
@@ -1,3 +1,6 @@
+#ifndef _poligono_h
+#define _poligono_h
+
 #include <stdio.h>
 #include <math.h>
 #include <string.h>
@@ -52,11 +55,38 @@ float string_to_enum(char* nome){
     return -1;
 }
 
-//TO-DO: Criar switch e case para cada enum, modificar o parametro de entrada.
+void* cria(enum_poli nome, int* values){
+    if(nome==CIR){
+        TCIR* cir = (TCIR*)malloc(sizeof(TCIR));
+        cir->raio = values[0];
+        return cir;
+    }else if(nome==QUA){
+        TQUA* qua = (TQUA*)malloc(sizeof(TQUA));
+        qua->lado=values[0];
+        return qua;
+    }else if(nome==RET){
+        TRET* ret = (TRET*)malloc(sizeof(TRET));
+        ret->base=values[0];
+        ret->altura=values[1];
+        return ret;
+    }else if(nome==TRI){
+        TTRI* tri = (TTRI*)malloc(sizeof(TTRI));
+        tri->base=values[0];
+        tri->altura=values[1];
+        return tri;
+    }else if(nome==TRA){
+        TTRA* tra = (TTRA*)malloc(sizeof(TTRA));
+        tra->base_menor=values[0];
+        tra->base_maior=values[1];
+        tra->altura=values[2];
+        return  tra;
+    }
+    return NULL;
+}
+
+
 //Calcula area para um determinado poligono, se caso esse poligono nao existir, retorna -1.
 float calcula_area(void* poligono, enum_poli nome){
-    TCIR* teste = (TCIR*)malloc(sizeof(TCIR));
-    TCIR* teste2;
     if(nome==CIR){
         TCIR* cir = poligono;
         return M_PI*pow(cir->raio, 2);
@@ -74,37 +104,6 @@ float calcula_area(void* poligono, enum_poli nome){
         return ((tra->base_menor+tra->base_maior)*tra->altura)/2;
     }
     return -1;
-
-    /* switch (nome)
-    {
-       case 1:
-         TCIR* cir = (TCIR*)malloc(sizeof(TCIR));
-         cir = poligono;
-         area = M_PI*pow(cir->raio, 2);
-       break;
-
-       case QUA:
-         TQUA* qua = (TQUA*) poligono;
-         area = pow(qua->lado, 2);
-       break;
-
-       case RET:
-         struct retangulo* ret = (struct retangulo*) poligono;
-         area = ret->base*ret->altura;
-       break;
-
-       case TRI:
-         TTRI* tri = (TTRI*) poligono;
-         area = (tri->base*tri->altura)/2;
-       break;
-
-       case TRA:
-         TTRA* tra = (TTRA*) poligono;
-         area = ((tra->base_menor+tra->base_maior)*tra->altura)/2;
-       break;
-
-       default:
-         area = -1;
-    }
-    return area;*/
 }
+
+#endif
